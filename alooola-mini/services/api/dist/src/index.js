bf2bc1fda2dae37a35185f3c0a495b65cb483e2d
natalie-a-1 <natalie.a.hill-1@ou.edu>
2026-01-19 22:03:31 -0600

    Remove all dist files from the API service, including routes, middleware, and modules, to clean up the project structure.

--- alooola-mini/services/api/dist/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Project source file.
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const server_1 = require("./server");
-void (0, server_1.startServer)();
